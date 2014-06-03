5636a00aab81071b4ce92333164b4d2807a09361
Maciej Sobkowski <maciejjo@maciejjo.pl>
2014-06-03 16:59:43 +0200

    main file cleanup

--- src/sokoban.c
+++ src/sokoban.c
@@ -67,44 +67,8 @@ int main()
   // rysowany.
   glEnable(GL_DEPTH_TEST);
 
-  // Tu się zaczynają ciekawe rzeczy.
-
-  // Na wstępie: W OpenGlu jak się chce coś zapisać na karcie graficznej,
-  // to trzeba to robić przez tzw. obiekty. Wszystkie funkcje które się
-  // zaczynają od glGen* służą do tworzenia takich obiektów. Działa to
-  // jak otwieranie plików open'em - dostaje się deskryptor, którego się
-  // potem używa do odwołania się do danego obiektu.
-
-  // VAO to Vertex Array Object. Działa to tak, że zapamiętują się w nim
-  // powiązania między wierzchołkami, a ich atrybutami (które są za chwilę)
-  // Dlatego wystarczy aktywować VAO, wczytać atrybuty wierzchołków, potem można
-  // aktywować inny vao, wczytać atrybuty innego modelu/czegokolwiek, i jak się
-  // aktywuje znowu ten pierwszy, to te atrybuty wczytane wcześniej będą ciągle
-  // wczytane.
-
-  // Typy zmiennych w OGL'u nazywają się zawsze GLtyp. Tutaj jest GLuint czyli
-  // unsigned int. Do niego chcemy uzyskać deskryptor obiektu.
-  GLuint vao;
-  // 1 oznacza, że chcemy stworzyć 1 obiekt. Do tego podajemy adres gdzie ma się
-  // zapisać deskryptor. Czyli adres zmiennej vao.
-  glGenVertexArrays(1, &vao);
-  // W OpenGLu żeby używać jakiegoś obiektu, trzeba go zbindować (używając
-  // deskryptora).
-  glBindVertexArray(vao);
-
-  // VBO to Vertex Buffer Object - czyli obiekt w którym się trzyma wierzchołki
-  // na karcie graficznej. Nie trzyma się tam tylko współrzędnych wierzchołka,
-  // ale też kolor, współrzędne textur i cokolwiek związane z wierzchołkiem.
-  GLuint vbo;
-  glGenBuffers(1, &vbo);
-
-  // Żeby pisać do bufora, trzeba go zbindować. Bindujemy do targetu GL_ARRAY_BUFFER
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  // Właściwy zapis danych na GPU. Tutaj nie używamy deskryptora bufora, tylko
-  // zapisujemy do obecnie zbindowanego bufora do GL_ARRAY_BUFFER.
-  // dalej podajemy rozmiar danych, adres danych, i rodzaj rysowania
-  // (do optymalizacji pamięci)
- // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  // Ustawienie koloru czyszczenia ekranu
+  glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
 
   // ***** Kompilacja shaderów *****
@@ -123,30 +87,6 @@ int main()
   glLinkProgram(shader_program);
   glUseProgram(shader_program);
   // ******************************
-
-
-  /*
-  // ***** Informacja dla shadera jak są ułożone dane w buforze *****
-  GLint posAttrib = glGetAttribLocation(shader_program, "position");
-  glEnableVertexAttribArray(posAttrib);
-  // Tu precyzujemy ułożenie danych. Podajemy jaki atrybut (position), ile
-  // ma danych (3 - x,y,z), jakiego typu (float), czy trzeba je normalizować
-  // (GL_FALSE - czyli nie), ile zajmuje 1 wiersz danych (8, bo są x,y,z,r,g,b,xt,yt)
-  // jakie jest przesunięcie względem początku wiersza (0, bo x,y,z są na początku)
-  glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 0);
-
-  GLint colAttrib = glGetAttribLocation(shader_program, "color");
-  glEnableVertexAttribArray(colAttrib);
-  // Tu zmienione przesunięcie, bo r,g,b są po 3 wartościach: x,y,z
-  glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-      (void *)(3 * sizeof(float)));
-
-  GLint texAttrib = glGetAttribLocation(shader_program, "texcoord");
-  glEnableVertexAttribArray(texAttrib);
-  glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-      (void *) (6 * sizeof(float)));
-  // ***************************************************************
-  */
 
   /*
   // *****  Ładowanie tekstur *****
@@ -186,28 +126,6 @@ int main()
   */
   // ******************************
 
-
-
-  // ***** Matematyka *****
-  // Zapisujemy do shadera macierze przez które będziemy mnożyć wierzchołki.
-  // Są to M - model, V - view, P - projection. Pomnożenie przez M przekłada
-  // ze współrzędnych modelu na współrzędne świata (obracanie, skalowanie,
-  // przesuwanie). V obraca i przesuwa świat żeby znalazł się przed kamerą.
-  // P tworzy perspektywę. Po pomnożeniu przez P jest wykonywane podzielenie
-  // wszystkich współrzędnych przez czwartą współrzędną, co daje nam NDC,
-  // czyli Normalized Device Coordinates.
-
-  // Nie modyfikujemy współrzędnych modelu - więc do macierzy M
-  // przypisujemy macierz jednostkową.
-
-  // Funkcja LookAt tworzy za nas macierz V, trzeba tylko podać
-  // współrzędne oka, punktu na który patrzymy, i wektor wyznaczający
-  // górę.
-
-
-  // Funkcja PerspectiveProjection tworzy macierz P. Przyjmuje
-  // kąt widzenia, aspect ratio, oraz położenie płaszczyzn
-  // przycinania z_near i z_far.
   kmMat4 proj;
   kmMat4PerspectiveProjection(&proj, 45.0f, (float) WIN_WIDTH / (float) WIN_HEIGHT, 1.0f, 100.0f);
   GLint uniProj = glGetUniformLocation(shader_program, "proj");
@@ -215,16 +133,10 @@ int main()
   glUniformMatrix4fv(uniProj, 1, GL_FALSE, &proj.mat[0]);
   // *********************
 
-  // Ustawienie koloru czyszczenia ekranu
-  glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-
   kmVec3 p_eye = { 0.1f, 5.0f, 0.0f };
   kmVec3 p_ctr = { 0.0f, 0.0f, 0.0f };
   kmVec3 p_up  = { 0.0f, 1.0f, 0.0f };
-
   kmMat4 view;
-  //kmMat4 pitch_rotation_mat;
-
   kmMat4LookAt(&view, &p_eye, &p_ctr, &p_up);
   GLint uniView = glGetUniformLocation(shader_program, "view");
   glUniformMatrix4fv(uniView, 1, GL_FALSE, &view.mat[0]);
